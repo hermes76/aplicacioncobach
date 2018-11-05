f74300978643baee5ab9499aa426234e15991c94
hermes76 <44623908+hermes76@users.noreply.github.com>
2018-11-04 19:35:45 -0600

    project creation

---
+++ aplicacioncobach.backend/Models/LocalDataContext.cs
@@ -0,0 +1,10 @@
+﻿
+namespace aplicacioncobach.backend.Models
+{
+    using domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<aplicacioncobach.common.models.Products> Products { get; set; }
+    }
+}
---
+++ aplicacioncobach.common/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿
+namespace aplicacioncobach.common.models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvaliable { get; set; }
+
+        public DateTime PublisOn { get; set; }
+
+    }
+}
